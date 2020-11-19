c15763ad5f4ed0add5df23c5f5eccd2002fb7d16
yujinlee325 <yu_jin325@naver.com>
2020-11-19 16:16:47 +0900

    1119_06

--- practice/practice/main.c
+++ practice/practice/main.c
@@ -7,29 +7,27 @@
 //
 
 #include<stdio.h>
-#include <stdlib.h>
 #include<string.h>
 
 
 int main(int argc, const char * argv[]) {
  
-    int i ;
-    FILE *fp;
+    FILE *fp ;
+    //char c;
     char str[100];
-    
-    
-    fp = fopen("sample.txt", "w");
+
+    fp = fopen("sample.txt", "r");
     if (fp == NULL)
     {
-        return -1 ;
+        return -1;
     }
     
-    for(i=0;i<3;i++)
-    {
-      printf("Input a word : ") ;
-      scanf("%s", str) ;
-      fprintf(fp,"%s\n",  str);
+   // while ((c=fgetc(fp))!= EOF){
+    while (fgets(str, 100, fp) != NULL){
+       // printf("%c", c);
+        printf("%s", str) ;
     }
+        
     
     fclose(fp);
     
