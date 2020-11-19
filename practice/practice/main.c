535593499c0ae885ab13f89183a7f5576bb6968e
yujinlee325 <yu_jin325@naver.com>
2020-11-19 14:55:42 +0900

    1119_01

---
+++ practice/practice/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  practice
+//
+//  Created by 이유진 on 2020/11/19.
+//  Copyright © 2020 이유진. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+ 
+    char code1 = 'A' ;
+    int  code2 = 65  ;
+    
+    printf("code1 = %c, (%d)\n", code1, code1);
+    printf("code2 = %c, (%d)\n", code2, code2);
+    
+    return 0;
+}
